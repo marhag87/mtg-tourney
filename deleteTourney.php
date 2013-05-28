63a14715142e620bd58b003dfed805de694b3237
Martin Hagström <marhag87@gmail.com>
2013-05-28 17:50:20 +0000

    Procedurize deleteTourney.php
    Fixes issue #6

--- deleteTourney.php
+++ deleteTourney.php
@@ -1,25 +1,17 @@
 <?PHP
-			header ("Content-Type:text/html; charset=utf-8");  
-			include 'dbconnect.php';
-			
-			dbConnect();
-			
-			//---------------------------------------------------------------------------------------------------------------
-			// Search Query!
-			//---------------------------------------------------------------------------------------------------------------
+	header ("Content-Type:text/html; charset=utf-8");  
+	include 'dbconnect.php';
+	
+	dbConnect();
+	
+	//---------------------------------------------------------------------------------------------------------------
+	// Search Query!
+	//---------------------------------------------------------------------------------------------------------------
 
-			$tourney_id = filter_var($_GET['tourney_id'],FILTER_VALIDATE_INT);
+	$tourney_id = filter_var($_GET['tourney_id'],FILTER_VALIDATE_INT);
 
-			$querystring="DELETE FROM tourney_player where tourney_id = ".$tourney_id.";";
-			$result=mysql_query($querystring);
-			if (!$result) err("\nSQL Query Error: ".mysql_error(),"Conference filter database querying error");
-			
-			$querystring="DELETE FROM tourney_deck where tourney_id = ".$tourney_id.";";
-			$result=mysql_query($querystring);
-			if (!$result) err("\nSQL Query Error: ".mysql_error(),"Conference filter database querying error");
-			
-			$querystring="DELETE FROM tourney where id = ".$tourney_id.";";
-			$result=mysql_query($querystring);
-			if (!$result) err("\nSQL Query Error: ".mysql_error(),"Conference filter database querying error");
-			echo "ok";
+	$querystring="call deleteTourney(".$tourney_id.");";
+	$result=mysql_query($querystring);
+	if (!$result) err("\nSQL Query Error: ".mysql_error(),"Conference filter database querying error");
+	echo "ok";
 ?>
